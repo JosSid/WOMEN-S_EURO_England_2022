d37bbb69069d7764f8e89bed14988f835357ba94
Josep Valls <alfredvays@gmail.com>
2022-07-20 16:54:41 +0200

    Añado el sorteo de equipos

---
+++ pruebas.js
@@ -0,0 +1,15 @@
+const team = {
+    name: 'Albalat',
+    matchesPlayed: 0,
+    matchesWon: 0,
+    matchesDraw: 0,
+    matchesLost: 0,
+    points: 0,
+    goalsFor: 0,
+    goalsAgainst: 0,
+    goalsDifference: 0
+}
+
+
+
+
